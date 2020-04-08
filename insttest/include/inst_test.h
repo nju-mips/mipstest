edbe68d2687607819a905f0bd3610b8ad8ad5d45
ouxianfei <ouxianfei@smail.nju.edu.cn>
2020-04-08 17:02:50 +0800

    n98_madd, n99_maddu, n100_msub, n101_msubu

--- insttest/include/inst_test.h
+++ insttest/include/inst_test.h
@@ -1298,3 +1298,112 @@ inst_error:
     mtc0 v1, c0_cause; \
     1: b 1b; \
     nop
+
+/* 96 */
+#define TEST_LL(data, base_addr, offset, offset_align, ref) \
+    LI (t1, data); \
+    LI (t0, base_addr); \
+    LI (v1, ref); \
+    sw t1, offset_align(t0); \
+    addiu a0, t0, 4; \
+    addiu a1, t0, -8; \
+    sw a0, offset_align(a0); \
+    sw a1, offset_align(a1); \
+    ll v0, offset(t0); \
+    lw a1, offset_align(a0); \
+    lw a0, offset_align(a1); \
+    lw a2, offset_align(a1); \
+    bne v0, v1, inst_error; \
+    nop
+
+/* 97 */
+#define TEST_SC(data, base_addr, offset, offset_align, ref) \
+    LI (t1, data); \
+    LI (t0, base_addr); \
+    LI (v1, ref); \
+    sc t1, offset(t0); \
+    addiu a0, t0, 4; \
+    addiu a1, t0, -4; \
+    sw a0, offset(a0); \
+    sw a1, offset(a1); \
+    lw v0, offset_align(t0); \
+    lw a0, offset(a1); \
+    lw a1, offset(a0); \
+    lw a2, offset(a1); \
+    bne v0, v1, inst_error; \
+    nop; \
+    li  v1, 1; \
+    bne t1, v1, inst_error; \
+    nop
+
+/* 98 */
+#define TEST_MADD(in_lo, in_hi, in_a, in_b, ref_lo, ref_hi) \
+    li t0, in_lo; \
+    mtlo t0; \
+    li t1, in_hi; \
+    mthi t1; \
+    li t0, in_a; \
+    li t1, in_b; \
+    madd t0, t1; \
+    mflo s5; \
+    mfhi s6; \
+    li v0, ref_lo; \
+    li v1, ref_hi; \
+    bne v0, s5, inst_error; \
+    nop; \
+    bne v1, s6, inst_error; \
+    nop
+
+/* 99 */
+#define TEST_MADDU(in_lo, in_hi, in_a, in_b, ref_lo, ref_hi) \
+    li t0, in_lo; \
+    mtlo t0; \
+    li t1, in_hi; \
+    mthi t1; \
+    li t0, in_a; \
+    li t1, in_b; \
+    maddu t0, t1; \
+    mflo s5; \
+    mfhi s6; \
+    li v0, ref_lo; \
+    li v1, ref_hi; \
+    bne v0, s5, inst_error; \
+    nop; \
+    bne v1, s6, inst_error; \
+    nop
+
+/* 100 */
+#define TEST_MSUB(in_lo, in_hi, in_a, in_b, ref_lo, ref_hi) \
+    li t0, in_lo; \
+    mtlo t0; \
+    li t1, in_hi; \
+    mthi t1; \
+    li t0, in_a; \
+    li t1, in_b; \
+    msub t0, t1; \
+    mflo s5; \
+    mfhi s6; \
+    li v0, ref_lo; \
+    li v1, ref_hi; \
+    bne v0, s5, inst_error; \
+    nop; \
+    bne v1, s6, inst_error; \
+    nop
+
+/* 101 */
+#define TEST_MSUBU(in_lo, in_hi, in_a, in_b, ref_lo, ref_hi) \
+    li t0, in_lo; \
+    mtlo t0; \
+    li t1, in_hi; \
+    mthi t1; \
+    li t0, in_a; \
+    li t1, in_b; \
+    msubu t0, t1; \
+    mflo s5; \
+    mfhi s6; \
+    li v0, ref_lo; \
+    li v1, ref_hi; \
+    bne v0, s5, inst_error; \
+    nop; \
+    bne v1, s6, inst_error; \
+    nop
